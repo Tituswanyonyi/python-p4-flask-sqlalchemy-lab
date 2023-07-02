f8b0fb562b54032107b1c8e9782c50325d5d1aaf
titus <titus.unavailable@student.moringaschool.com>
2023-07-02 13:41:08 +0300

    Assignment complete

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, make_response
 from flask_migrate import Migrate
 
@@ -13,21 +11,56 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+
 @app.route('/')
 def home():
     return '<h1>Zoo app</h1>'
 
+
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+    response_body = f'''
+            <ul>ID:{animal.id}</ul> 
+            <ul>Name:{animal.name}</ul>
+            <ul>Species:{animal.species}</ul>
+            <ul>Enclosure: {animal.enclosure_id}</ul>
+            <ul>Zookeeper: {animal.zookeeper_id}</ul>
+
+                 
+    '''
+    response = make_response(response_body, 200)
+    return response
+
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    response_body = f'''
+            <ul>ID:{zookeeper.id}</ul> 
+            <ul>Name:{zookeeper.name}</ul>
+            <ul>Birthday:{zookeeper.birthday}</ul>
+            <ul>Animal:{zookeeper.animals}</ul>
+            
+                 
+    '''
+    response = make_response(response_body, 200)
+    return response
+
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    response_body = f'''
+            <ul>ID:{enclosure.id}</ul> 
+            <ul>Environment:{enclosure.environment}</ul>
+            <ul>Birthday:{enclosure.open_to_visitors}</ul>
+            <ul>Animal:{enclosure.animals}</ul>
+            <ul>Open to Visitors:{enclosure.open_to_visitors}</ul>
+                 
+    '''
+    response = make_response(response_body, 200)
+    return response
 
 
 if __name__ == '__main__':
